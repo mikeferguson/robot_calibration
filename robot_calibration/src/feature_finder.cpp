70bce801e6b290afe8992f792011cdc3e27a2b76
narora1 <niharika1712@gmail.com>
2015-10-28 18:27:47 -0700

    finder added

--- robot_calibration/src/feature_finder.cpp
+++ robot_calibration/src/feature_finder.cpp
@@ -21,6 +21,7 @@
 #include <robot_calibration/capture/checkerboard_finder.h>
 #include <robot_calibration/capture/ground_plane_finder.h>
 #include <robot_calibration/capture/gripper_depth_finder.h>
+#include <robot_calibration/capture/gripper_color_finder.h>
 
 namespace robot_calibration
 {
@@ -84,9 +85,16 @@ bool loadFeatureFinders(ros::NodeHandle& nh,
       finder.reset(new robot_calibration::CheckerboardFinder(finder_handle));
     }
     else if (type == "robot_calibration/GripperDepthFinder")
-    {
+    {std::cout << "buzzzz" << std::endl;
       ROS_INFO("  New robot_calibration/GripperDepthFinder: %s", name.c_str());
       finder.reset(new robot_calibration::GripperDepthFinder(finder_handle));
+    }
+    else if (type == "robot_calibration/GripperColorFinder")
+    {
+      ROS_INFO("  New robot_calibration/GripperColorFinder: %s", name.c_str());
+      std::cout << "hi" << std::endl;
+      finder.reset(new robot_calibration::GripperColorFinder(finder_handle));
+      std::cout << "hi1" << std::endl;
     }
     else
     {
