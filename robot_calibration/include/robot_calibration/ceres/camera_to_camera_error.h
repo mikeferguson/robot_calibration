0bf860bf3ae26d14a1f23936fa822b89740ae8d5
Niharika Arora <niharika1712@gmail.com>
2017-12-04 15:31:27 -0800

    Adds plane calibration

---
+++ robot_calibration/include/robot_calibration/ceres/camera_to_camera_error.h
@@ -0,0 +1,186 @@
+/*
+ * Copyright (C) 2015 Fetch Robotics Inc.
+ * Copyright (C) 2013-2014 Unbounded Robotics Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless repts_matuired by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: Niharika Arora
+
+#ifndef ROBOT_CALIBRATION_CERES_CAMERA_TO_CAMERA_ERROR_H
+#define ROBOT_CALIBRATION_CERES_CAMERA_TO_CAMERA_ERROR_H
+
+#include <string>
+#include <ceres/ceres.h>
+#include <robot_calibration/calibration_offset_parser.h>
+#include <robot_calibration/models/camera3d.h>
+#include <robot_calibration/models/chain.h>
+#include <robot_calibration_msgs/CalibrationData.h>
+
+#include <opencv2/calib3d/calib3d.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+namespace robot_calibration
+{
+
+/**
+ *  \brief Model of a camera on another camera chain.
+ */
+struct CameraToCameraError
+{
+    /**
+     *  \brief Error block for calibrating a 3d camera to a single arm.
+     *  \param camera1_model The model for the first camera, used for reprojection.
+     *  \param camera2_model The model for the second camera, used for reprojection.
+     *  \param free_param_info Helper container for processing the free parameters.
+     *  \param data The calibration data collected
+     */
+    CameraToCameraError(Camera3dModel* camera1_model,
+        Camera3dModel* camera2_model,
+        CalibrationOffsetParser* offsets,
+        robot_calibration_msgs::CalibrationData& data)
+    {
+      camera1_model_ = camera1_model;
+      camera2_model_ = camera2_model;
+      offsets_ = offsets;
+      data_ = data;
+    }
+
+    virtual ~CameraToCameraError() {}
+
+    /**
+     *  \brief Operator called by CERES optimizer.
+     *  \param free_params The offsets to be applied to joints/transforms.
+     *  \param residuals The residuals computed, to be returned to the optimizer.
+     */
+    bool operator()(double const * const * free_params,
+        double* residuals) const
+    {
+      // Update calibration offsets based on free params
+      offsets_->update(free_params[0]);
+
+      // Project the first camera observations
+      std::vector <geometry_msgs::PointStamped> camera1_pts =
+          camera1_model_->project(data_, *offsets_);
+
+      // Project the second camera estimation
+      std::vector <geometry_msgs::PointStamped> camera2_pts =
+          camera2_model_->project(data_, *offsets_);
+
+      // Calculate plane normals using SVD
+
+      std::vector <cv::Point3f> camera1_points;
+      std::vector <cv::Point3f> camera2_points;
+
+      for (size_t i = 0; i < camera1_pts.size(); ++i)
+      {
+        camera1_points.push_back(cv::Point3f(camera1_pts[i].point.x, camera1_pts[i].point.y, camera1_pts[i].point.z));
+      }
+      for (size_t i = 0; i < camera2_pts.size(); ++i)
+      {
+        camera2_points.push_back(cv::Point3f(camera2_pts[i].point.x, camera2_pts[i].point.y, camera2_pts[i].point.z));
+      }
+
+      // Find the planes
+      cv::Mat plane_1 = getPlane(camera1_points);
+      cv::Mat plane_2 = getPlane(camera2_points);
+
+      // Compute the residuals by minimizing the normals of the calculated planes
+      residuals[0] = std::fabs(plane_1.at<float>(0, 0)) - std::fabs(plane_2.at<float>(0, 0));
+      residuals[1] = std::fabs(plane_1.at<float>(0, 1)) - std::fabs(plane_2.at<float>(0, 1));
+      residuals[2] = std::fabs(plane_1.at<float>(0, 2)) - std::fabs(plane_2.at<float>(0, 2));
+
+      return true;  // always return true
+    }
+
+    /**
+     *  \brief Helper factory function to create a new error block. Parameters
+     *         are described in the class constructor, which this function calls.
+     *  \tparam num_points The number of points in the observation, this forms the
+     *          size of the residuals.
+     *  \tparam num_free_params The number of free parameters being used for
+     *          joint and link calibration.
+     */
+    static ceres::CostFunction* Create(Camera3dModel* camera1_model,
+        Camera3dModel* camera2_model,
+        CalibrationOffsetParser* offsets,
+        robot_calibration_msgs::CalibrationData& data)
+    {
+      int index = -1;
+      for (size_t k = 0; k < data.observations.size() ; k++)
+      {
+        if ( data.observations[k].sensor_name == camera2_model->name())
+        {
+          index = k;
+          break;
+        }
+      }
+
+      if (index == -1)
+      {
+        std::cerr << "Sensor name doesn't match any of the existing finders" << std::endl;
+        return 0;
+      } 
+
+      ceres::DynamicNumericDiffCostFunction<CameraToCameraError> * func;
+      func = new ceres::DynamicNumericDiffCostFunction<CameraToCameraError>(
+          new CameraToCameraError(camera1_model, camera2_model, offsets, data));
+      func->AddParameterBlock(offsets->size());
+      func->SetNumResiduals(3);
+
+      return static_cast<ceres::CostFunction*>(func);
+    }
+
+    /**
+     *  \brief Does a plane fit to the poinst and calculates the normals
+     *  \tparam points The points sampled from the point cloud
+     *  \treturn The normals
+     */
+    cv::Mat getPlane(std::vector<cv::Point3f> points) const
+    {
+      // Calculate centroid
+      cv::Point3f centroid(0,0,0);
+      for(size_t i =0; i< points.size() ; i++)
+      {
+        centroid += points.at(i);
+      }
+
+      centroid.x = centroid.x / points.size();
+      centroid.y = centroid.y / points.size();
+      centroid.z = centroid.z / points.size();
+
+      // subtract centroid from all points
+      cv::Mat pts_mat(points.size(), 3, CV_32F);
+      for(size_t i =0; i< points.size() ; i++)
+      {
+        pts_mat.at<float>(i,0) = points[i].x - centroid.x;
+        pts_mat.at<float>(i,1) = points[i].y - centroid.y;
+        pts_mat.at<float>(i,2) = points[i].z - centroid.z;
+      }
+
+      // SVD
+      cv::SVD svd(pts_mat, cv::SVD::FULL_UV);
+      cv::Mat normal = svd.vt.row(2);
+
+      return normal;
+    }
+
+    Camera3dModel * camera1_model_;
+    Camera3dModel * camera2_model_;
+    CalibrationOffsetParser * offsets_;
+    robot_calibration_msgs::CalibrationData data_;
+};
+
+}  // namespace robot_calibration
+
+#endif  // ROBOT_CALIBRATION_CERES_CAMERA_TO_CAMERA_ERROR_H
--- robot_calibration/src/plane_finder.cpp
+++ robot_calibration/src/plane_finder.cpp
@@ -15,9 +15,13 @@
  * limitations under the License.
  */
 
-#include <robot_calibration/capture/ground_plane_finder.h>
+// Author: Niharika Arora
+
+#include <robot_calibration/capture/plane_finder.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
 #include <math.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace robot_calibration
 {
@@ -26,31 +30,35 @@ const unsigned X = 0;
 const unsigned Y = 1;
 const unsigned Z = 2;
 
-GroundPlaneFinder::GroundPlaneFinder(ros::NodeHandle & nh) :
-  FeatureFinder(nh),
-  waiting_(false)
+PlaneFinder::PlaneFinder(ros::NodeHandle & nh) :
+    FeatureFinder(nh),
+    waiting_(false)
 {
   std::string topic_name;
   nh.param<std::string>("topic", topic_name, "/points");
   subscriber_ = nh.subscribe(topic_name,
                              1,
-                             &GroundPlaneFinder::cameraCallback,
+                             &PlaneFinder::cameraCallback,
                              this);
 
-  nh.param<std::string>("camera_sensor_name", camera_sensor_name_, "cameraground");
-  nh.param<std::string>("chain_sensor_name", chain_sensor_name_, "ground");
+  nh.param<std::string>("camera_sensor_name", camera_sensor_name_, "camera");
   nh.param<double>("points_max", points_max_, 60);
+  nh.param<double>("min_y", min_y_, 0);
+  nh.param<double>("max_y", max_y_, 0);
+  nh.param<double>("min_z", min_z_, 0);
   nh.param<double>("max_z", max_z_, 0);
+  nh.param<std::string>("transform_frame", transform_frame_, "base_link");
 
-  publisher_ = nh.advertise<sensor_msgs::PointCloud2>("ground_plane_points", 10);
+  publisher_ = nh.advertise<sensor_msgs::PointCloud2>("plane_points", 10);
   if (!depth_camera_manager_.init(nh))
   {
     // Error will be printed in manager
     throw;
   }
+
 }
 
-void GroundPlaneFinder::cameraCallback(const sensor_msgs::PointCloud2& cloud)
+void PlaneFinder::cameraCallback(const sensor_msgs::PointCloud2& cloud)
 {
   if (waiting_)
   {
@@ -59,7 +67,7 @@ void GroundPlaneFinder::cameraCallback(const sensor_msgs::PointCloud2& cloud)
   }
 }
 
-bool GroundPlaneFinder::waitForCloud()
+bool PlaneFinder::waitForCloud()
 {
   ros::Duration(1/10.0).sleep();
 
@@ -79,8 +87,7 @@ bool GroundPlaneFinder::waitForCloud()
   return !waiting_;
 }
 
-
-bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
+bool PlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
 {
   geometry_msgs::PointStamped rgbd;
   geometry_msgs::PointStamped world;
@@ -96,22 +103,57 @@ bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
   sensor_msgs::PointCloud2ConstIterator<float> xyz(cloud_, "x");
   sensor_msgs::PointCloud2Iterator<float> iter(cloud_, "x");
 
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+  geometry_msgs::TransformStamped transformStamped;
+  try
+  {
+
+    transformStamped = tfBuffer.lookupTransform("base_link", cloud_.header.frame_id,
+                             ros::Time(0), ros::Duration(10.0));
+  }
+  catch (tf2::TransformException ex)
+  {
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();
+  }
+
   size_t j = 0;
   for (size_t i = 0; i < num_points; i++)
   {
-    geometry_msgs::Point p;
-    p.x = (xyz + i)[X];
-    p.y = (xyz + i)[Y];
-    p.z = (xyz + i)[Z];
+    geometry_msgs::PointStamped p;
+    p.point.x = (xyz + i)[X];
+    p.point.y = (xyz + i)[Y];
+    p.point.z = (xyz + i)[Z];
+    p.header.stamp = ros::Time(0);
+    p.header.frame_id = cloud_.header.frame_id;
 
-    if (!std::isfinite(p.x) || !std::isfinite(p.y) || !std::isfinite(p.z))
+    geometry_msgs::PointStamped p_out;
+    try
+    {
+      tfBuffer.transform(p, p_out, transform_frame_);
+    }
+    catch (tf2::TransformException ex)
+    {
+      ROS_ERROR("%s", ex.what());
+      ros::Duration(1.0).sleep();
+    }
+
+    // Remove the NaNs in the point cloud
+    if (!std::isfinite(p.point.x) || !std::isfinite(p.point.y) || !std::isfinite(p.point.z))
       continue;
 
-    if (p.z == 0)
+    // Remove the points immediately in front of te camera in the point cloud
+    // NOTE : This is to handle sensors that publish zeros instead of NaNs in the point cloud
+    if (p.point.z == 0)
       continue;
 
-    if (p.z >max_z_ && max_z_ != 0)
+    // Transformed point in the transformed frame (default is base_link))
+    if (p_out.point.z < min_z_ or p_out.point.z > max_z_ or p_out.point.y < min_y or p_out.point.y > max_y)
+    {
       continue;
+    }
+
     (iter + j)[X] = (xyz + i)[X];
     (iter + j)[Y] = (xyz + i)[Y];
     (iter + j)[Z] = (xyz + i)[Z];
@@ -135,17 +177,13 @@ bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
   sensor_msgs::PointCloud2Modifier cloud_mod(cloud);
   cloud_mod.setPointCloud2FieldsByString(1, "xyz");
   cloud_mod.resize(points_total);
-  sensor_msgs::PointCloud2Iterator<float> iter_cloud(cloud_, "x");
+  sensor_msgs::PointCloud2Iterator<float> iter_cloud(cloud, "x");
 
   // Set msg size
   int idx_cam = msg->observations.size() + 0;
-  int idx_chain = msg->observations.size() + 1;
-  msg->observations.resize(msg->observations.size() + 2);
+  msg->observations.resize(msg->observations.size() + 1);
   msg->observations[idx_cam].sensor_name = camera_sensor_name_;
-  msg->observations[idx_chain].sensor_name = chain_sensor_name_;
-
   msg->observations[idx_cam].features.resize(points_total);
-  msg->observations[idx_chain].features.resize(points_total);
 
   size_t step = cloud_.width/(points_total);
   size_t k = 0;
@@ -158,11 +196,6 @@ bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
 
   for (size_t i = 0; i < points.size(); i++)
   {
-    // for ground plane world can just be zero as we are concerned only with z
-    world.point.x = 0;
-    world.point.y = 0;
-    world.point.z = 0;
-
     // Get 3d point
     int index = static_cast<int>(points[i].x);
     rgbd.point.x = (xyz + index)[X];
@@ -170,7 +203,6 @@ bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
     rgbd.point.z = (xyz + index)[Z];
     msg->observations[idx_cam].features[i] = rgbd;
     msg->observations[idx_cam].ext_camera_info = depth_camera_manager_.getDepthCameraInfo();
-    msg->observations[idx_chain].features[i] = world;
 
     iter_cloud[0] = rgbd.point.x;
     iter_cloud[1] = rgbd.point.y;
@@ -178,6 +210,8 @@ bool GroundPlaneFinder::find(robot_calibration_msgs::CalibrationData * msg)
     ++iter_cloud;
   }
 
+  // Publish debug info
+  msg->observations[idx_cam].cloud = cloud_;
   publisher_.publish(cloud);
   return true;
 }
