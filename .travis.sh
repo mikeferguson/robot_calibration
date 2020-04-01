716c18142f17b2174865a25a5f17b201178bc48c
Michael Ferguson <mfergs7@gmail.com>
2020-03-31 20:02:26 -0400

    fixes

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Setup environment correctly so that CI actually runs
 ci_env=`bash <(curl -s https://codecov.io/env)`
 DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $ci_env"
+
+echo "DOCKER_RUN_OPTS=$DOCKER_RUN_OPTS"
 
 # Call into industrial CI
 source .industrial/.travis.sh
