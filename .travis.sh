170c1bc0c87203aced1c63a589e21e5d7b0488f7
Michael Ferguson <mfergs7@gmail.com>
2020-03-31 20:17:40 -0400

    try this another way

--- .travis.sh
+++ .travis.sh
@@ -6,5 +6,8 @@ DOCKER_RUN_OPTS="$DOCKER_RUN_OPTS $ci_env"
 
 echo "DOCKER_RUN_OPTS=$DOCKER_RUN_OPTS"
 
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Call into industrial CI
-source $(dirname ${BASH_SOURCE:-$0})/.industrial_ci/.travis.sh
+source ${THIS_DIR}/.industrial_ci/.travis.sh
